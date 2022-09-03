98ec5107d1d4a27bfbf173c1404efe9f28de9b5e
danilbushkov <danil1357997@gmail.com>
2022-09-03 15:53:19 +0300

    added merge sort for linked list

--- data_structures/list/src/lib.rs
+++ data_structures/list/src/lib.rs
@@ -51,5 +51,36 @@ mod tests {
         let mut list = List::from(&vec![1]);
         list.back_to_front();
         assert_eq!(list.to_vec(), vec![1]);
+
+
+
+        let mut list = List::<usize>::new();
+        list.sort();
+        assert_eq!(list.to_vec(), vec![]);
+
+        let mut list = List::from(&vec![1]);
+        list.sort();
+        assert_eq!(list.to_vec(), vec![1]);
+
+        let mut list = List::from(&vec![2, 1]);
+        assert_eq!(list.size(), 2);
+        list.sort();
+        assert_eq!(list.to_vec(), vec![1, 2]);
+
+        let mut list = List::from(&vec![2, 1, 0]);
+        assert_eq!(list.size(), 3);
+        list.sort();
+        assert_eq!(list.to_vec(), vec![0, 1, 2]);
+
+        let mut list = List::from(&vec![2, 1, 0, 1]);
+        assert_eq!(list.size(), 4);
+        list.sort();
+        assert_eq!(list.to_vec(), vec![0, 1, 1, 2]);
+
+        let mut left = List::from(&vec![2, 1, 0, 1, 100, 3, 400, -100, 19, 2, 5, 0, 7]);
+        let mut right = vec![2, 1, 0, 1, 100, 3, 400, -100, 19, 2, 5, 0, 7];
+        left.sort();
+        right.sort();
+        assert_eq!(left.to_vec(), right);
     }
 }
--- data_structures/list/src/list.rs
+++ data_structures/list/src/list.rs
@@ -2,7 +2,7 @@
 
 
 struct Node<T> 
-    where T: Clone
+    where T: Clone + PartialOrd
 {
     data: T,
     next: Option<Box<Node<T>>>
@@ -11,18 +11,19 @@ struct Node<T>
 
 
 pub struct List<T> 
-    where T: Clone
+    where T: Clone + PartialOrd
 {
     start: Option<Box<Node<T>>>,
+    size: usize,
 }
 
 impl<T> List<T> 
-    where T: Clone
+    where T: Clone + PartialOrd
 {
     pub fn new() -> Self {
         Self {
             start: None,
-            
+            size: 0,
 
         }
     }
@@ -32,7 +33,7 @@ impl<T> List<T>
     {
         let mut list = Self {
             start: None, 
-            
+            size: 0,
         };
         
         let mut iter = vec.iter();
@@ -41,6 +42,7 @@ impl<T> List<T>
                 data: n.clone(),
                 next: None,
             }));
+            list.size += 1;
         }
         let mut tmp = &mut list.start;
         while let Some(n) = iter.next() {
@@ -49,6 +51,7 @@ impl<T> List<T>
                     data: n.clone(),
                     next: None,
                 }));
+                list.size += 1;
                 tmp = &mut t.next;
             }
         }
@@ -79,14 +82,17 @@ impl<T> List<T>
             data: value,
             next: node,
         }));
+        self.size += self.size + 1;
     }
 
     pub fn pop_front(&mut self) -> Option<T> {
         let node = self.start.take();
         if let Some(n) = node {
             self.start = n.next;
+            self.size += self.size - 1;
             return Some(n.data);
         }
+        
         None
     }
 
@@ -106,5 +112,138 @@ impl<T> List<T>
         self.start = node;
         
     }
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    pub fn sort(&mut self) {
+
+        
+        self.start = Self::merge_sort(0, self.size as isize - 1, self.start.take()).0;
+
+    }
+
+    fn merge_sort(begin: isize, end: isize, node: Option<Box<Node<T>>>) -> (
+        Option<Box<Node<T>>>, Option<Box<Node<T>>>) {
+        let mut node = node;
+        
+        if begin >= end {
+            let mut next: Option<Box<Node<T>>> = None;
+            if let Some(n) = &mut node {
+                next = n.next.take();
+            }
+            return (node, next);
+        }
+        let middle = (begin + end) / 2;
+        let (mut node_a, next) = Self::merge_sort(begin, middle, node);
+        let (mut node_b, next_list) = Self::merge_sort(middle+1, end, next);
+
+
+        
+
+        let mut n = middle - begin + 1;
+        let mut m = end - middle;
+        let mut start: Option<Box<Node<T>>> = None;
+        let mut end: &mut Option<Box<Node<T>>> = &mut None;
+        
+
+        if let Some(a) = node_a {
+            if let Some(b) = node_b {
+                if a.data <= b.data {
+                    let mut tmp = a;
+                    node_a = tmp.next.take();
+                    start = Some(tmp);
+                    end = &mut start;
+                    n -= 1;
+                    node_b = Some(b);
+
+                } else {
+                    let mut tmp = b;
+                    node_b = tmp.next.take();
+                    start = Some(tmp);
+                    end = &mut start;
+                    m -= 1;
+                    node_a = Some(a);
+
+                }
+            } else {
+                let mut tmp = a;
+                node_a = tmp.next.take();
+                start = Some(tmp);
+                end = &mut start;
+                n -= 1;
+            }
+            
+        }
+
+        while n > 0 && m > 0 {
+            if let Some(a) = node_a {
+                if let Some(b) = node_b {
+                    if a.data <= b.data {
+                        if let Some(e) = end {
+                            let mut tmp = a;
+                            node_a = tmp.next.take();
+                            e.next = Some(tmp);
+                            end = &mut e.next;
+                            node_b = Some(b);
+                            n -= 1;
+                        } else {
+                            
+                            node_a = Some(a);
+                            node_b = Some(b);
+                        }
+                    } else {
+                        if let Some(e) = end {
+                            let mut tmp = b;
+                            node_b = tmp.next.take();
+                            e.next = Some(tmp);
+                            end = &mut e.next;
+                            node_a = Some(a);
+                            m -= 1;
+                        } else {
+                            
+                            node_a = Some(a);
+                            node_b = Some(b);
+                        }
+                    }
+                } else {
+                    node_a = Some(a);
+                    //panic!("{}", n);
+                    //m -= 1;
+                }
+            }
+        }
+        
+        
+        while n > 0 {
+            if let Some(mut a) = node_a {
+                if let Some(e) = end {
+                    node_a = a.next.take();
+                    e.next = Some(a);
+                    end = &mut e.next;
+                    n -= 1;
+                } else {
+                    node_a = Some(a);
+                }
+            }
+            
+        }
+    
+        while m > 0 {
+            if let Some(mut b) = node_b {
+                if let Some(e) = end {
+                    node_b = b.next.take();
+                    e.next = Some(b);
+                    end = &mut e.next;
+                    m -= 1;
+                } else {
+                    node_b = Some(b);
+                }
+            }
+            
+        }
+        (start, next_list)
+    }
+
     
 }
