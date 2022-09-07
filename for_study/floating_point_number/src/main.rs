861cdbfa90c6bcf4f43eb695a52618dfede71cbc
danilbushkov <danil1357997@gmail.com>
2022-09-07 18:37:35 +0300

    added floating_point_number

---
+++ for_study/floating_point_number/src/main.rs
@@ -0,0 +1,50 @@
+use num::pow;
+
+
+fn main() {
+
+
+    let m: u32 = 1 << 31;
+
+    let a: u32 = 0b1_10000001_11111111111111111111111;
+
+
+
+
+    println!("{:}", binary_to_f64(a));
+
+}
+
+fn binary_to_f64(binary_number: u32) -> f64 {
+    // let sign = sign(binary_number);
+    // let exponent = exponent(binary_number);
+    // let fraction = fraction(binary_number);
+
+    // sign * fraction 
+    0.0
+}
+
+
+
+fn sign(binary_number: u32) -> f64 {
+    let bitmask: u32 = 1 << 31;
+    match binary_number & bitmask {
+        0 => 1.0,
+        _ => -1.0,
+    }
+}
+
+fn exponent(binary_number: u32) -> i32 {
+    let bitmask: u32 = 1 << 31;
+    let exponent = (binary_number & !bitmask) >> 23;
+
+    (exponent as i32) - 128
+}
+
+fn fraction(binary_number: u32) -> f64 {
+    let bitmask: u32 = 0b1_11111111 << 23;
+    let number = (binary_number & !bitmask) as f64;
+    let fraction: f64 = number / pow(2.0, 23);
+
+    fraction
+}
