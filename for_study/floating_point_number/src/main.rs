851ef522aa95a62c75a578b92828db9f50b3cb40
danilbushkov <danil1357997@gmail.com>
2022-09-18 19:26:56 +0300

    added number input and output

--- for_study/floating_point_number/src/main.rs
+++ for_study/floating_point_number/src/main.rs
@@ -1,95 +1,53 @@
-
-
+use floating_point_number::f64_to_b32;
+use floating_point_number::b32_to_f64;
+use std::env;
+use std::io;
 
 fn main() {
 
 
-    let m: u32 = 1 << 31;
+    // let m: u32 = 1 << 31;
 
-    let a: u32 = 0b1_01111111_11111111111111111111111;
+    // let a: u32 = 0b1_01111111_11111111111111111111111;
 
-    println!("{:}", b32_to_f64(a));
-    println!("{:b}", f64_to_b32(b32_to_f64(a)));
+    // println!("{:}", b32_to_f64(a));
+    // println!("{:b}", f64_to_b32(b32_to_f64(a)));
+    let args: Vec<String> = env::args().collect();
 
-
-}
-
-fn f64_to_b32(number: f64) -> u32 {
-    let sign = number.signum();
-    let mut number = number.abs();
-
-    let mut exponent: i32 = 128;
-
-    if number >= 1.0 {
-        while number >= 1.0 {
-            number /= 2.0;
-            exponent +=1;
-        }
-    } else if number < 0.5 {
-        while number < 0.5 {
-            number *= 2.0;
-            exponent -=1;
-        }
+    //b32 or f64
+    let mut arg = &"".to_string();
+    if args.len() > 1 {
+        arg = &args[1];
     }
-    let mut fraction: u32 = 0;
-    for _ in 0..22 {
-        number *= 2.0;
-        if number > 1.0 {
-            number -= 1.0;
-            fraction = fraction + 1;
-        } 
-        fraction = fraction << 1;
-    } 
-    number *= 2.0;
-    if number > 0.0 {
-        fraction = fraction + 1;
-    } 
+    
 
-    let sign: u32 = if sign > 0.0 {
-        0
+    let mut input = String::new();
+    if let Err(e) = io::stdin().read_line(&mut input) {
+        panic!("Error: {:?}", e);
+    }
+    input.retain(|c| c != '\n');
+
+    if arg == "b32" {
+        match input.parse::<f64>() {
+            Ok(n) => {
+                println!("{:b}", f64_to_b32(n));
+            },
+            Err(e) => {
+                panic!("Error: {:?}", e);
+            }
+        }
     } else {
-        1 << 31
-    };
-    
-
-    let exponent = (exponent as u32) << 23;
-    
-
-    sign | exponent | fraction
-}
-
-
-
-
-fn b32_to_f64(binary_number: u32) -> f64 {
-    let sign = sign(binary_number);
-    let exponent = exponent(binary_number);
-    let fraction = fraction(binary_number);
-
-    sign * fraction * 2.0_f64.powi(exponent)
-}
-
-
-
-fn sign(binary_number: u32) -> f64 {
-    let bitmask: u32 = 1 << 31;
-    match binary_number & bitmask {
-        0 => 1.0,
-        _ => -1.0,
+        match u32::from_str_radix(&input, 2) {
+            Ok(n) => {
+                println!("{}", b32_to_f64(n));
+            },
+            Err(e) => {
+                panic!("Error: {:?}", e);
+            }
+        }
     }
+    //println!("Searching for {}", query);
+    //println!("In file {}", file_path);
+
 }
 
-fn exponent(binary_number: u32) -> i32 {
-    let bitmask: u32 = 1 << 31;
-    let exponent = (binary_number & !bitmask) >> 23;
-
-    (exponent as i32) - 128
-}
-
-fn fraction(binary_number: u32) -> f64 {
-    let bitmask: u32 = 0b1_11111111 << 23;
-    let number = (binary_number & !bitmask) as f64;
-    let fraction: f64 = number / 2.0_f64.powi(23);
-
-    fraction
-}
